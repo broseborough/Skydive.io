ed20d5c9a1ada3ad38dfc545eca8d4bf82da5ea0
Braden Roseborough <broseborough@lynda.com>
2013-06-10 00:01:45 -0700

    adds google analytics and adds to the non-functional settings screen

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 var main = angular.module('main', []).
 	config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
-		when('/settings', {templateUrl: 'partials/settings.html', controller:window.settingsController}).
-		when('/about', {templateUrl: 'partials/about.html', controller:null}).
-		when('/forecast', {templateUrl: 'partials/forecast.html', controller:window.forecastController}).
-		otherwise({redirectTo:'/forecast'});
+			when('/settings', {templateUrl: 'partials/settings.html', controller:window.settingsController}).
+			when('/about', {templateUrl: 'partials/about.html', controller:null}).
+			when('/forecast', {templateUrl: 'partials/forecast.html', controller:window.forecastController}).
+			otherwise({redirectTo:'/forecast'});
 	}]);
 main = main;
